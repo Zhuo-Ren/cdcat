4f914fa5cce367c3d12cc2b221f6e140f412f826
XuYang <xuyangng_mail@sina.cn>
2020-10-30 16:36:06 +0800

    ntree输出为括号表达式（丢了一个文件）

---
+++ test_unit/plug_in/output/output_ntree_to_string_bracker_form.py
@@ -0,0 +1,9 @@
+"""
+this is the unit test of input_from_string_bracket_form.
+"""
+from nlp_plantform.plug_in.input.ntree_from_string_bracket_form import input_ntree_from_string_bracket_form
+from nlp_plantform.plug_in.output.ntree_to_string_bracket_form import output_ntree_to_string_bracket_form
+
+ntree = input_ntree_from_string_bracket_form('(S (NP (D the) (N dog)) (VP (V chased) (NP (D the) (N cat))))')
+out = output_ntree_to_string_bracket_form(ntree)
+print(out)
