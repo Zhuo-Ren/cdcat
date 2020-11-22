f4cc7e46ad786a89dd3c496cab6b2de8d5b5d3d7
Zhuo-Ren <52392665+Zhuo-Ren@users.noreply.github.com>
2020-11-22 17:32:36 +0800

    修改instance构造函数和pool.add_instance()函数的单元测试。

--- test_unit/center/instancetest.py
+++ test_unit/center/instancetest.py
@@ -1,37 +1,35 @@
 from nlp_plantform.center.instance import Instance
-from nlp_plantform.center.instancepool import  InstancePool
+from nlp_plantform.center.instancepool import InstancePool
 
+# 测试Instance的构造函数
 i1 = Instance()
 i2 = Instance()
 i3 = Instance()
+print(i1, i1.instance_pool)
+print(i2, i2.instance_pool)
+print(i3, i3.instance_pool)
+
+# 测试InstancePool的构造函数
 p = InstancePool()
-#初始的时候p为空
-print('*********')
-print(i1)
-print(i2)
-print(i3)
 print(p)
-print('*********')
-print('生成测试')
-print('*********')
-i1 = p.add_instance(i1)
-i2 = p.add_instance(i2)
-i3 = p.add_instance(i3)
-print(i1)
-print(i2)
-print(i3)
-print('*********')
-print('测试字典')
-dict1 = {"desc":"国航"}
-dict2 = {"desc":"天津航空"}
-dict3 = {"desc":"厦航"}
-i4 = p.add_instance(dict1)
-i5 = p.add_instance(dict2)
-i6 = p.add_instance(dict3)
-print(i4)
-print(i5)
-print(i6)
-print(i1.instance_pool)
+
+# 测试InstancePool(instance_obj)
+p.add_instance(i1)
+p.add_instance(i2)
+p.add_instance(i3)
+print(i1, i1.instance_pool)
+print(i2, i2.instance_pool)
+print(i3, i3.instance_pool)
+print(p)
+
+# 测试InstancePool(dict_obj)
+i4 = p.add_instance({"desc":"国航"})
+i5 = p.add_instance({"desc":"天津航空"})
+i6 = p.add_instance({"desc":"厦航"})
+print(i4, i4.instance_pool)
+print(i5, i5.instance_pool)
+print(i6, i6.instance_pool)
+print(p)
 
 
 
