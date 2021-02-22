8d9ad1740f30dddfc3a4156f15fe68d22caad5a1
Cain-w <ustbwangzhenyu@163.com>
2021-02-22 15:41:46 +0800

    解决不引入cdcat.py报错问题

--- test_unit/plug_in/manual_annotation_tool/cdcat/instancepool_to_dict.py
+++ test_unit/plug_in/manual_annotation_tool/cdcat/instancepool_to_dict.py
@@ -1,12 +1,8 @@
 from nlp_plantform.config import data_path
 from nlp_plantform.center.instancepool import InstancePool
 import _pickle as cPickle
-from nlp_plantform.plug_in.manual_annotation_tool.cdcat.cdcat import cdcat
+from nlp_plantform.plug_in.input.instances_from_pickle import input_instances_from_pickle
 
-def input_instances_from_pickle(path: str = data_path + r'/instances.pkl') -> InstancePool:
-    with open(path, 'rb') as pkl_file:
-        instances = cPickle.load(pkl_file)
-    return instances
 
 ip = input_instances_from_pickle(data_path + r"/instances.pkl")
 info = ip.to_dict()
