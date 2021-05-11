c5e02c32c552e692f03693b02e69831cfeb2e459
XuYang <xuyangng_mail@sina.cn>
2021-05-11 10:59:45 +0800

    debug：优化项目根目录的判定逻辑

--- nlp_platform/config.py
+++ nlp_platform/config.py
@@ -1,5 +1,7 @@
 import os
 
-root_path = os.path.abspath(os.path.dirname(__file__)).split('cdcat')[0]
+root_path = os.path.abspath(os.path.dirname(__file__)).split('\\cdcat\\')[0]
+root_path += "\\"
+
 root_path = r"%scdcat"%root_path
 data_path = r"%s/data"%root_path
