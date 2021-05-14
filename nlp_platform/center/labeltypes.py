29b5c33458e23c1031303c680c8423e96e2e46af
XuYang <xuyangng_mail@sina.cn>
2021-05-14 21:05:26 +0800

    初步的labeltypes.py

--- nlp_platform/center/labeltypes.py
+++ nlp_platform/center/labeltypes.py
@@ -2,192 +2,169 @@ from typing import Dict, List, Tuple, Union  # for type hinting
 import copy
 
 
-class Label(object):
+class Label(dict):
     """
-    A base class of label type
+    A base class of label type.
 
     Attributes:
         owner: AAA
         key: AAA
-        config: AAA
         value: AAA
     """
+    def __init__(self, owner, config):
+        """
+        XXX
 
-    from nlp_platform.center.labels import InstanceLabels, NodeLabels
-
-    def __init__(self, owner: Union[NodeLabels,InstanceLabels], key, value=None):
+        :param owner:
+        :type owner: Union[
+            nlp_platform.center.node.Node,
+            nlp_platform.center.instance.Instance
+          ]
+        :param config: Configuration of this label. {"key": "XXXX"}
+        :type config: dict
+        """
         # param check: owner
-        from nlp_platform.center.labels import InstanceLabels, NodeLabels
-        if not isinstance(owner, (NodeLabels, InstanceLabels)):
-            raise TypeError("param 'owner' should be InstanceLabels obj or NodeLabels obj.")
-        # param check: key
-        if not isinstance(key, str):
-            raise TypeError("param 'key' should be a string.")
-        # param check: value
-        pass  # in child class
+        from nlp_platform.center.node import Node
+        from nlp_platform.center.instance import Instance
+        if not isinstance(owner, (Node, Instance)):
+            raise TypeError("param 'owner' should be Instance obj or Node obj.")
+        # param check: config
+        if not isinstance(config, dict):
+            raise TypeError("param 'config' should be a dict.")
+        if "key" not in config:
+            raise RuntimeError("A 'key' is required in param 'config'")
 
-        # public: owner and owner_labels
-        self.owner = owner
+        # public: ["owner"]
+        self["owner"] = owner
 
-        # public: key
-        self.key = key
+        # public: ["key"]
+        self["key"] = config["key"]
 
-        # private: value
-        self._value = copy.copy(self.empty_value)
-        if value is not None:
-            self.value = value
-            "这里将调用子类中定义的value的setter，将包含同步功能如果需要的话。"
 
-    # public: config
-    @property
-    def config(self) -> Dict:
+class SimpleLabel(Label):
+    def __init__(self, owner, config):
         """
-        The config dict of this label according to config_label_sys.json.
+        init.
 
-        :return: The config dict of this label according to config_label_sys.json.
+        :param owner:
+        :type owner: Union[
+            nlp_platform.center.node.Node,
+            nlp_platform.center.instance.Instance
+          ]
+        :param config: Configuration of this label. {"key": "XXXX"}
+        :type config: dict
         """
-        return self.owner_labels.config[self.key]
+        # 父类的初始化
+        super().__init__(owner, config)
 
-    # public: owner_labels
-    @property
-    def owner_labels(self):
-        return self.owner
+        # PRELIMINARY_CODE
+        if "PRELIMINARY_CODE" in config:
+            eval(config["PRELIMINARY_CODE"])
 
-    # public: owner_obj
-    @property
-    def owner_obj(self):
-        return self.owner.owner
+        # public;["value_init"]
+        self["value_init"] = config["value_init"]
 
-    # public: value
-    @property
-    def value(self):
-        return self._value
-    @value.setter
-    def value(self,value):
-        self._value = value  # 如有需要，子类应该添加linkedLabel同步机制
+        # public: ["value_type_hint"]
+        if "value_type_hint" in config:
+            self["value_type_hint"] = config["value_type_hint"]
 
-    def value_empty(self) -> None:
+        # public: ["value_optional"]
+        if "value_optional" in config:
+            self["value_optional"] = config["value_optional"]
+
+        # public: ["value"]
+        value = eval(" t = " + self["value_init"])
+        if "value_type_hint" in self:
+            if not isinstance(value, eval(self["value_type_hint"])):
+                raise TypeError("Value of this label do not match the type hint.")
+        if "value_optional" in self:
+            if value not in self["value_optional"] :
+                raise TypeError("Value of this label do not match the options.")
+        self["value"] = value
+
+
+class ListLabel(Label):
+    def __init__(self, owner, config):
         """
-        Del the label value. **And the linked label of this label will be changed synchronously.**
+        init.
 
-        :return: None
+        :param owner:
+        :type owner: Union[
+            nlp_platform.center.node.Node,
+            nlp_platform.center.instance.Instance
+          ]
+        :param config: Configuration of this label. {"key": "XXXX"}
+        :type config: dict
         """
-        self.value = copy.copy(self.empty_value)
+        # 父类的初始化
+        super().__init__(owner, config)
 
-    def sync_add(self, value):
-        self.value = value
+        # PRELIMINARY_CODE
+        if "PRELIMINARY_CODE" in config:
+            eval(config["PRELIMINARY_CODE"])
 
-    def sync_del(self, value):
-        # sync check
-        if self.value != value:
-            raise RuntimeError
-        #
-        self._value = copy.copy(self.empty_value)
+        # public;["value_init"]
+        self["value_init"] = config["value_init"]
 
-    def readable(self):
-        return self.value
+        # public: ["value_type_hint"]
+        if "value_type_hint" in config:
+            self["value_type_hint"] = config["value_type_hint"]
 
-    def ajax_process(self, ajax_param, root_node, instance_pool):
-        pass
+        # public: ["value_optional"]
+        if "value_optional" in config:
+            self["value_optional"] = config["value_optional"]
 
-    def to_info(self):
-        info_dict = []
-        for v in self._value:
-            info_dict.append(v.position())
-        return info_dict
+        # public: ["value"]
+        value = eval(" t = " + self["value_init"])
+        if "value_type_hint" in self:
+            if not isinstance(value, eval(self["value_type_hint"])):
+                raise TypeError("Value of this label do not match the type hint.")
+        if "value_optional" in self:
+            if value not in self["value_optional"] :
+                raise TypeError("Value of this label do not match the options.")
+        self["value"] = value
 
 
-class SimpleLabel(LabelType):
-    def __init__(self, owner, key, value: Union[None, str] = None):
-        # public
-        self.empty_value = None
-        #
-        super().__init__(owner=owner, key=key, value=value)
-
-    from nlp_platform.center.nodetree import NodeTree
-    from nlp_platform.center.instancepool import InstancePool
-
-    def ajax_process(self,
-                     ajax_param: str,
-                     root_node: NodeTree = None,
-                     instance_pool: InstancePool = None):
+class RelationLabel(Label):
+    def __init__(self, owner, config):
         """
-        This function process ajax require which try to change the label value.
+        init.
 
-        :param ajax_param: The relative params in ajax require.
-            String "" will be converted into None which means this label is to be
-            delete( because null and undefined in js convert into "" in python).
-        :param root_node: root node of all the node.
-        :param instance_pool: instance pool that manages all the instance.
-        :return: True if process is success, a string describe the error if process is failed
+        :param owner:
+        :type owner: Union[
+            nlp_platform.center.node.Node,
+            nlp_platform.center.instance.Instance
+          ]
+        :param config: Configuration of this label. {"key": "XXXX"}
+        :type config: dict
         """
-        if str == "":
-            self.value_empty()
-            del self.owner_labels[self.config["key"]]
-        else:
-            self.value = ajax_param
+        # 父类的初始化
+        super().__init__(owner, config)
 
+        # PRELIMINARY_CODE
+        if "PRELIMINARY_CODE" in config:
+            eval(config["PRELIMINARY_CODE"])
 
-class ListLabel(LabelType):
-    def __init__(self, owner, key, value: Union[None, List[str]]=None):
-        # public
-        self.empty_value = []
-        #
-        super().__init__(owner, key, value)
+        # public;["value_init"]
+        self["value_init"] = config["value_init"]
 
-    from nlp_platform.center.nodetree import NodeTree
-    from nlp_platform.center.instancepool import InstancePool
+        # public: ["value_type_hint"]
+        if "value_type_hint" in config:
+            self["value_type_hint"] = config["value_type_hint"]
 
-    def ajax_process(self,
-                     ajax_param: str,
-                     root_node: NodeTree = None,
-                     instance_pool: InstancePool = None):
-        """
-        This function process ajax require which try to change the label value.
+        # public: ["value_optional"]
+        if "value_optional" in config:
+            self["value_optional"] = config["value_optional"]
 
-        :param ajax_param: The relative params in ajax require.
-            String "" will be converted into None which means this label is to be
-            delete( because null and undefined in js convert into "" in python).
-        :param root_node: root node of all the node.
-        :param instance_pool: instance pool that manages all the instance.
-        :return: True if process is success, a string describe the error if process is failed
-        """
-        if str == "":
-            self.value_empty()
-            del self.owner_labels[self.config["key"]]
-        else:
-            self.value = ajax_param
-
-
-class RelationLabel(LabelType):
-    def __init__(self, owner, key, value: Union[None, str]=None):
-        # public: empty_value
-        self.empty_value = None
-        #
-        super().__init__(owner, key, value)
-
-    from nlp_platform.center.nodetree import NodeTree
-    from nlp_platform.center.instancepool import InstancePool
-
-    def ajax_process(self,
-                     ajax_param: str,
-                     root_node: NodeTree = None,
-                     instance_pool: InstancePool = None):
-        """
-        This function process ajax require which try to change the label value.
-
-        :param ajax_param: The relative params in ajax require.
-            String "" will be converted into None which means this label is to be
-            delete( because null and undefined in js convert into "" in python).
-        :param root_node: root node of all the node.
-        :param instance_pool: instance pool that manages all the instance.
-        :return: True if process is success, a string describe the error if process is failed
-        """
-        if str == "":
-            self.value_empty()
-            del self.owner_labels[self.config["key"]]
-        else:
-            self.value = ajax_param
+        # public: ["value"]
+        value = eval(" t = " + self["value_init"])
+        if "value_type_hint" in self:
+            if not isinstance(value, eval(self["value_type_hint"])):
+                raise TypeError("Value of this label do not match the type hint.")
+        if "value_optional" in self:
+            if value not in self["value_optional"] :
+                raise TypeError("Value of this label do not match the options.")
+        self["value"] = value
 
 
 label_types = {
