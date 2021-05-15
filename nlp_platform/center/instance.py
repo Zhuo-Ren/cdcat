bebbdf26ebcae911490343d8edd74b77ca0db89c
XuYang <xuyangng_mail@sina.cn>
2021-05-15 14:51:56 +0800

    center的初版

--- nlp_platform/center/instance.py
+++ nlp_platform/center/instance.py
@@ -2,13 +2,45 @@ from typing import Dict, List, Tuple, Union  # for type hinting
 
 
 class Instance(dict):
+    """
+    属性:
+     - Instance.config
+     - instance.instance_pool
+     - node["标签"]
+    """
     # static
     config = {}
+    """
+        for example::
+        {
+            "PRELIMINARY_CODE": "from random import randint \n ",
+            "LABELS": {
+                "id": {
+                    "type": "SimpleLabel",
+                    "value_type_hint": "str",
+                    "value_init": "'%03d' % randint(0, 999)",
+                },
+                "desc": {
+                    "type": "SimpleLabel",
+                    "value_type_hint": "str",
+                    "value_init": "self['id']"
+                },
+                "type": {
+                    "type": "SimpleLabel",
+                    "value_type_hint": "str",
+                    "value_init": "none",
+                    "value_optional": ["none", "entity", "event"]
+                }
+            }
+        }
+    """
 
-    def __init__(self, instance_pool=None, info: Dict = None):
+    def __init__(self, pool=None, info: Dict = None):
+        super().__init__()
+
         # param check: instance_pool
         from nlp_platform.center.instancepool import InstancePool
-        if (instance_pool is not None) & (not isinstance(instance_pool, InstancePool)):
+        if (pool is not None) & (not isinstance(pool, InstancePool)):
             raise TypeError
 
         # param check: info
@@ -18,28 +50,22 @@ class Instance(dict):
             raise TypeError("param label_dict should be None or a dict.")
 
         # public: instance_pool
-        self.instance_pool = instance_pool
+        self.instance_pool = pool
 
         # labels
-        from nlp_platform.center.labeltypes import labeltypes
-        for key in self.config:
-            label_config = self.config[key]
-            self[key] = labeltypes[label_config[key]["type"]](label_config)
+        from nlp_platform.center.labeltypes import label_types
+        for label_key, label_config in self.config["LABELS"].items():
+            self[label_key] = label_types[label_config["type"]](label_config)
 
-
-    def to_info(self, include_id_label=True, include_fixed_label=True):
+    def to_info(self):
         r = {}
-        if self._labels is not None:
-            r.update(self.labels.to_info())
-        if include_id_label is False:
-            del r["id"]
-        if include_fixed_label is False:
-            del r["desc"]
+        for label_key in self:
+            r.update(self[label_key].to_info())
         return r
 
 
 # 加载配置
 import json
 with open('./label_config.json', 'r', encoding='utf8') as f:
-    label_config = json.load(f)
-Instance.config = label_config["Instance"]
+    config = json.load(f)
+Instance.config = config["Instance"]
