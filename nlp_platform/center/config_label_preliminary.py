33e96b7e1e9922a3dbcd8b5cce092a2f34e37f11
XuYang <xuyangng_mail@sina.cn>
2021-05-24 10:10:55 +0800

    node的id的检查逻辑：“n:XX/XX/XX.raw:3-4;9-10”

---
+++ nlp_platform/center/config_label_preliminary.py
@@ -0,0 +1,26 @@
+from random import randint
+
+
+def node_id_check(s):
+    import re
+    r = re.findall(r'^n:[\\r"/"0-9a-zA-Z\u4e00-\u9fa5]+:([;\d-]+)', s)
+    if r == []:
+        return False
+    r = re.findall(r"(\d+)-(\d+);?", str(r))
+    old = 0
+    for i in r:
+        for j in i:
+            new = int(j)
+            if old <= new:
+                old = new
+            else:
+                return False
+    return True
+
+
+# s = "n:dds\\fn中国w/eds/2\\k你5/好gn:3-18;19-20;21-22;77-96;98-115;117-119"  # 符号都不要
+# print(node_id_check(s))
+# s = "n:dds\\fn中国w/eds/2\\k你5/好gn:3-18;19-20;30-22;77-96;98-115;117-119"  # 符号都不要
+# print(node_id_check(s))
+# s = "n:dds\\fn中国w/eds/2\\k你5/好gn3-18;19-20;30-22;77-96;98-115;117-119"  # 符号都不要
+# print(node_id_check(s))
