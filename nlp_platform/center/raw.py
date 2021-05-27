18f1d4a88b7ad87cae6d3cf4c33eca344ac94c35
guozhijie <guozhijie_tjnu@163.com>
2021-05-27 12:36:24 +0800

    将raw.py加入项目，实现按照n:xxx/xxx/xx:0-3的格式查询索引指向的内容

--- nlp_platform/center/raw.py
+++ nlp_platform/center/raw.py
@@ -1,7 +1,33 @@
 class Raw(dict):
-    pass
-    # def __getitem__(self, k):
-    #     if k: # 符合格式
-    #         pass
-    #     else:
-    #         return super().__getitem__(k)
+    def __getitem__(self, key):
+        def dict_get(obj_dict, objkey, default):
+            tmp = obj_dict
+            for k, v in tmp.items():
+                if k == objkey:
+                    return v
+                else:
+                    if type(v) is dict:
+                        ret = dict_get(v, objkey, default)
+                        if ret is not default:
+                            return ret
+            return default
+
+        if key[:2] == 'n:':
+            search_content = []
+            search_path = key.split(':')[1]
+            search_index = key.split(':')[2]
+            index_list = search_index.split('-')
+            search_file = search_path.split('/')[-1]
+            obj_dict = self
+            # print(search_path)
+            ret = dict_get(obj_dict, search_file, None)
+            if int(index_list[0]) < len(ret) and int(index_list[1]) < len(ret):
+                for i in range(int(index_list[0]), int(index_list[1]) + 1):
+                    search_content.append(ret[i])
+                search_content = ''.join(search_content)
+                return search_content
+            else:
+                print("索引范围不正确")
+                return KeyError
+        else:
+            return self
