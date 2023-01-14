e304860a7606471e72be40c59e32c7ba474a62f5
Zhuo-Ren <52392665+Zhuo-Ren@users.noreply.github.com>
2023-01-14 17:47:16 +0800

    新增功能：plug_in/input/corpus_from_ecbpxml新增修改后缀的功能，例如读取1_10ecb.raw.txt文件(旧后缀为.raw.txt)，但读取后的文件名使用1_10ecb(新后缀为空)。 修改单元测试：针对上述功能修改单元测试

--- nlp_platform/plug_in/input/raw_from_tokenized_en2zh_text.py
+++ nlp_platform/plug_in/input/raw_from_tokenized_en2zh_text.py
@@ -6,18 +6,23 @@ from typing import Dict
 
 def raw_from_tokenized_en2zh_text(
         path: str,
-        fname_regex: str = ".xml.EN-ZH.txt$"
+        file_suffix: str = ".xml.EN-ZH.txt$",
+        file_suffix_new: str = ""
 ) -> Raw:
     """
     Extract pure text from a tokenized en2zh corpus and return a Raw_obj represent it.
 
-    就是抽取指定文本，并删除所有“|”（分词标记）。
+    迭代遍历path下的所有文件，对使用后缀file_suffix的文件，判定为tokenized en2zh file(简称en2zh文件)。
+    对en2zh文件，读取文本，并删除所有“|”（分词标记）。
+    按照path下的文件树结构，把读取结果保存为dict。
+    （注意对en2zh文件支持后缀修改，即原本以file_suffix为后缀，在输出的dict中可以改为以file_suffix_new为后缀）
 
     Example: <ref20221230213431>
 
     :param path: Path to a corpus (a txt file or a folder that contains txt
     file, those txt file contain tokenized en2zh text).
-    :param fname_regex: A regEx used to identify the tokenized en2zh text file.
+    :param file_suffix: Suffix of the tokenized en2zh text file.
+    :param file_suffix_new: In the returned Raw_obj, suffix of the tokenized en2zh file will be changed into this new suffix.
     :return: A Raw_obj which contains pure text of the corpus. Tokenize info is removed.
     """
     # param check: path
@@ -26,21 +31,28 @@ def raw_from_tokenized_en2zh_text(
     if not os.path.exists(path):
         raise TypeError
     #
-    return Raw(dict_from_tokenized_en2zh_text(path=path, fname_regex=fname_regex))
+    return Raw(dict_from_tokenized_en2zh_text(path=path, file_suffix=file_suffix))
 
 
 def dict_from_tokenized_en2zh_text(
         path: str,
-        fname_regex: str = ".xml.EN-ZH.txt$"
+        file_suffix: str = ".xml.EN-ZH.txt",
+        file_suffix_new: str = ""
 ) -> Dict[str, str]:
     """
     Extract pure text from a tokenized en2zh corpus and return a dict represent it.
+
+    迭代遍历path下的所有文件，对使用后缀file_suffix的文件，判定为tokenized en2zh file(简称en2zh文件)。
+    对en2zh文件，读取文本，并删除所有“|”（分词标记）。
+    按照path下的文件树结构，把读取结果保存为dict。
+    （注意对en2zh文件支持后缀修改，即原本以file_suffix为后缀，在输出的dict中可以改为以file_suffix_new为后缀）
 
     Example: <to20221230210752>
 
     :param path: Path to a corpus (a txt file or a folder that contains txt
     file, those txt file contain tokenized en2zh text).
-    :param fname_regex: A regEx used to identify the tokenized en2zh text file.
+    :param file_suffix: Suffix of the tokenized en2zh text file.
+    :param file_suffix_new: In the returned dict_obj, suffix of the tokenized en2zh file will be changed into this new suffix.
     :return: A dict which contains pure text of the corpus. Tokenize info is removed.
     """
     # param check: path
@@ -56,14 +68,15 @@ def dict_from_tokenized_en2zh_text(
         if os.path.isfile(cur_path):
             file_path = cur_path
             file_name = cur_name
-            if re.search(fname_regex, file_name, flags=0):
+            if re.search(file_suffix+"$", file_name, flags=0):
                 with open(file_path, 'r', encoding='utf8') as f:
                     text = f.read()
                     text = text.replace("|", "")
-                    raw[file_name] = text
+                    file_name_with_new_suffix = file_name[:(-1*len(file_suffix))] + file_suffix_new
+                    raw[file_name_with_new_suffix] = text
         # if dir
         elif os.path.isdir(cur_path):
             dir_path = cur_path
             dir_name = cur_name
-            raw[dir_name] = dict_from_tokenized_en2zh_text(path=dir_path, fname_regex=fname_regex)
+            raw[dir_name] = dict_from_tokenized_en2zh_text(path=dir_path, file_suffix=file_suffix)
     return raw
